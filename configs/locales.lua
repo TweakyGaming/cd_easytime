f196a6062a24586c0ccdf1b51e2b2359e58a566d
TweakyGaming <tweakymedia@gmail.com>
2022-03-20 09:23:47 +0100

    DE Local

--- configs/locales.lua
+++ configs/locales.lua
@@ -4,6 +4,11 @@ Locales = {
         ['drop_player'] = 'Did you know, that banging your head against a wall for one hour burns 150 calories. Alternatively, you can walk your dog for 45 minutes, which also burns 150 calories – and is much less painful. Dont cheat.',
     },
 
+    ['DE'] = {
+        ['invalid_permissions'] = 'Sie haben keine Berechtigung zur Verwendung dieses Befehls',
+        ['drop_player'] = 'Wussten Sie, dass man 150 Kalorien verbrennt, wenn man seinen Kopf eine Stunde lang gegen eine Wand schlägt? Alternativ können Sie auch 45 Minuten lang mit Ihrem Hund spazieren gehen, was ebenfalls 150 Kalorien verbrennt - und viel weniger schmerzhaft ist. Schummeln Sie nicht.',
+    },
+
     ['FR'] = {
         ['invalid_permissions'] = 'Vous ne disposez pas des autorisations nécessaires pour utiliser cette commande',
         ['drop_player'] = 'Did you know, that banging your head against a wall for one hour burns 150 calories. Alternatively, you can walk your dog for 45 minutes, which also burns 150 calories – and is much less painful. Dont cheat.',
